0c82535fc2fe665971adeec9f4f9d53d73997c76
Aksel Alpay <aksel.alpay@uni-heidelberg.de>
2020-02-17 16:47:30 +0100

    use sphinx w/ recommonmark

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+
+
+# Markdown support
+
+from recommonmark.parser import CommonMarkParser
+
+# The suffix of source filenames.
+source_suffix = ['.md']
+
+#source_parsers = {
+#	'.md': CommonMarkParser,
+#}
+
+extensions = ['recommonmark']
+
+master_doc = 'README'
